5327b148bdc76defdcd012223a87a6af8a21101a
Teppo Kurki <teppo.kurki@iki.fi>
2017-09-06 22:47:12 +0300

    chore: add test for RMC

---
+++ test/RMC.js
@@ -0,0 +1,28 @@
+const Bacon = require('baconjs')
+const assert = require('assert')    
+
+describe('RMC', function () {
+  it('works without datetime', done => {
+    const streams = {
+        'navigation.speedOverGround': new Bacon.Bus(),
+        'navigation.courseOverGroundTrue': new Bacon.Bus(),
+        'navigation.datetime': new Bacon.Bus(),
+        'navigation.position': new Bacon.Bus()
+    }
+    const app = {
+      streambundle: { getSelfStream: path => streams[path] },
+      emit: (event, value) => {
+          assert.equal(value, '$SKRMC,,A,0600.0000,N,00500.0000,E,1.9,114.6,,0.0,E*70')
+          done()
+      }
+    }
+    const plugin = require('../')(app)
+    const options = {
+      RMC: true
+    }
+    plugin.start(options)
+    app.streambundle.getSelfStream('navigation.speedOverGround').push('1')
+    app.streambundle.getSelfStream('navigation.courseOverGroundTrue').push('2')
+    app.streambundle.getSelfStream('navigation.position').push({longitude: 5, latitude: 6})
+  })
+})
