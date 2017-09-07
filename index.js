c3fb364cd69de8976c0ad401608483a25409b7c5
Teppo Kurki <teppo.kurki@iki.fi>
2017-09-07 17:04:34 +0300

    refactor: extract nmea.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Bacon = require('baconjs');
+const {toSentence, computeChecksum, toHexString, radsToDeg, padd, toNmeaDegrees} = require('./nmea')
+
 
 module.exports = function(app) {
   var plugin = {
@@ -392,76 +394,4 @@ module.exports = function(app) {
   });
 
   return plugin
-}
-
-
-function toSentence(parts) {
-  var base = parts.join(',');
-  return base + computeChecksum(base);
-}
-var m_hex = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F'
-];
-
-function computeChecksum(sentence) {
-  var c1;
-  var i;
-
-  // skip the $
-  i = 1;
-
-  // init to first character    var count;
-
-  c1 = sentence.charCodeAt(i);
-
-  // process rest of characters, zero delimited
-  for (i = 2; i < sentence.length; ++i) {
-    c1 = c1 ^ sentence.charCodeAt(i);
-  }
-
-  return '*' + toHexString(c1);
-};
-
-function toHexString(v) {
-  var lsn;
-  var msn;
-
-  msn = (v >> 4) & 0x0f;
-  lsn = (v >> 0) & 0x0f;
-  return m_hex[msn] + m_hex[lsn];
-};
-
-function radsToDeg(radians) {
-  return radians * 180 / Math.PI
-}
-
-function padd(n, p, c)
-{
-  var pad_char = typeof c !== 'undefined' ? c : '0';
-  var pad = new Array(1 + p).join(pad_char);
-  return (pad + n).slice(-pad.length);
-}
-
-function toNmeaDegrees(val)
-{
-  val = Math.abs(val)
-  var minutes = Math.floor(val)
-  var minutes_decimal = val % 1
-  minutes_decimal *= 60.0;
-  return padd(minutes.toFixed(0),2) + padd(minutes_decimal.toFixed(4), 7)
 }
