82d57f13226721fb8c5021665fb80f54863ad597
joabakk <github@heiamoss.com>
2018-02-16 10:36:39 +0100

    Add PSILCD1 and cleaned PSILTTBS

--- sentences/PSILTBS.js
+++ sentences/PSILTBS.js
@@ -1,15 +1,15 @@
 /*
-+PSILTBS - Proprietary target boat speed sentence for Silva => Nexus => Garmin displays
-+
-+
-+           0     1  2
-+           |     |  |
-+ $PSILTBS,XX.xx,N,*hh<CR><LF>
-+Field Number:
-+0 Target Boat speed in knots
-+1 N for knots
-+2 Checksum
-+*/
+PSILTBS - Proprietary target boat speed sentence for Silva => Nexus => Garmin displays
+
+
+           0     1  2
+           |     |  |
+ $PSILTBS,XX.xx,N,*hh<CR><LF>
+Field Number:
+0 Target Boat speed in knots
+1 N for knots
+2 Checksum
+*/
 
 const nmea = require('../nmea.js')
 module.exports = function (app) {
@@ -17,8 +17,7 @@ module.exports = function (app) {
     title: 'PSILTBS - Garmin proprietary target boat speed',
     keys: ['performance.targetSpeed'],
     f: function (tbs) {
-      var tbs_kn = tbs * 1.94384
-      return nmea.toSentence(['$PSILTBS', tbs_kn.toFixed(2), 'N'])
+      return nmea.toSentence(['$PSILTBS', nmea.msToKnots(tbs).toFixed(2), 'N'])
     }
   }
 }
