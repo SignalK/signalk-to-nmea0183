a2a35c00d8f41e360562147095377a2f24bd60c6
David Sanner <root@q64>
2018-05-06 14:32:29 -0700

    added magneticVariation to sentence

--- sentences/HDG.js
+++ sentences/HDG.js
@@ -9,13 +9,19 @@ const nmea = require('../nmea.js')
 module.exports = function (app) {
   return {
     title: 'HDG - Heading magnetic:.',
-    keys: ['navigation.headingMagnetic'],
-    f: function hdg (headingMagnetic) {
+    keys: ['navigation.headingMagnetic', 'navigation.magneticVariation' ],
+    f: function hdg (headingMagnetic, magneticVariation) {
+      var magneticVariationDir = 'E';
+      if ( headingMagnetic < 0 ) {
+        magneticVariationDir = 'W';
+        magneticVariation = magneticVariation * -1;
+      }
+
       return nmea.toSentence([
         '$IIHDG',
         nmea.radsToDeg(headingMagnetic).toFixed(2),
-        '',
-        '',
+        nmea.radsToDeg(magneticVariation).toFixed(2),
+        magneticVariationDir,
         '',
         ''
       ])
