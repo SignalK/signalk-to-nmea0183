8b0dd7c6c62baac11848d4c446daab5e0ce96f2f
David Sanner <dsanner@rigel.com>
2018-05-07 23:23:01 -0700

    returns HDT if headingTrue not vailable but headingMagnetic & magneticVariation are

--- sentences/HDT.js
+++ sentences/HDT.js
@@ -3,13 +3,26 @@ const nmea = require('../nmea.js')
 module.exports = function (app) {
   return {
     title: 'HDT - Heading True',
-    keys: ['navigation.headingTrue'],
-    f: function (heading) {
-      return nmea.toSentence([
-        '$IIHDT',
-        nmea.radsToDeg(heading).toFixed(1),
-        'T'
-      ])
+    keys: ['navigation.headingTrue', 'navigation.headingMagnetic', 'navigation.magneticVariation' ],
+    defaults: ['', '', ''],
+    f: function (heading, headingMagnetic, magneticVariation) {
+      if ( heading != '' ) {
+        return nmea.toSentence([
+          '$IIHDT',
+          nmea.radsToDeg(heading).toFixed(1),
+          'T'
+        ])
+      }
+      else if ( headingMagnetic != '' &&  magneticVariation != '') {
+        heading = headingMagnetic + magneticVariation;
+        if (heading > 2 * Math.PI) heading -= 2 * Math.PI
+        else if (heading < 0 ) heading += 2 * Math.PI
+        return nmea.toSentence([
+          '$IIHDT',
+          nmea.radsToDeg(heading).toFixed(1),
+          'T'
+        ])
+      }
     }
   }
 }
