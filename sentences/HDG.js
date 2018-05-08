e8b28e18b164600c244f628bafb71c8825dc5315
David Sanner <dsanner@rigel.com>
2018-05-07 22:19:14 -0700

    added defaults for HDG in case magneticVariation is not available

--- sentences/HDG.js
+++ sentences/HDG.js
@@ -10,17 +10,24 @@ module.exports = function (app) {
   return {
     title: 'HDG - Heading magnetic:.',
     keys: ['navigation.headingMagnetic', 'navigation.magneticVariation' ],
+    defaults: [undefined, ''],
     f: function hdg (headingMagnetic, magneticVariation) {
-      var magneticVariationDir = 'E';
-      if ( headingMagnetic < 0 ) {
-        magneticVariationDir = 'W';
-        magneticVariation = magneticVariation * -1;
+      if ( magneticVariation == '' ) {
+        var magneticVariationDir = '';
+      }
+      else {
+        var magneticVariationDir = 'E';
+        if ( headingMagnetic < 0 ) {
+          magneticVariationDir = 'W';
+          magneticVariation = magneticVariation * -1;
+        }
+        magneticVariation = nmea.radsToDeg(magneticVariation).toFixed(2);
       }
 
       return nmea.toSentence([
         '$IIHDG',
         nmea.radsToDeg(headingMagnetic).toFixed(2),
-        nmea.radsToDeg(magneticVariation).toFixed(2),
+        magneticVariation,
         magneticVariationDir,
         '',
         ''
