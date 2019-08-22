b40bb9b5392c7dd12e808866fd70e90012150139
free-x <oroitburd@gmail.com>
2019-08-22 10:28:27 +0200

    GGA test is working now

--- test/GGA.js
+++ test/GGA.js
@@ -14,14 +14,13 @@ describe('GGA', function() {
      app.streambundle.getSelfStream('navigation.position').push({ longitude: -122.03782631066667, latitude: 37.39109795066667 })
   })
 
-/*  it('works with sample values', done=> {
+  it('works with sample values', done=> {
      const onEmit = (event, value) => {
-        assert.equal(value ,'$IIGGA,172814.0,3723.46587704,N,12202.26957864,W,2,6,1.2,18.893,M,-25.669,M,2.0,0031*58')
+        assert.equal(value ,'$IIGGA,172814,3723.4659,N,12202.2696,W,2,6,1.2,18.893,M,-25.669,M,2,0031*53')
 	done()
      }
      const app = createAppWithPlugin(onEmit, 'GGA')
      app.streambundle.getSelfStream('navigation.datetime').push('2015-12-05T17:28:14Z')
-     app.streambundle.getSelfStream('navigation.position').push({ longitude: -122.03782631066667, latitude: 37.39109795066667 })
      app.streambundle.getSelfStream('navigation.gnss.methodQuality').push('DGNSS fix')
      app.streambundle.getSelfStream('navigation.gnss.satellites').push(6)
      app.streambundle.getSelfStream('navigation.gnss.horizontalDilution').push(1.2)
@@ -29,5 +28,6 @@ describe('GGA', function() {
      app.streambundle.getSelfStream('navigation.gnss.geoidalSeparation').push(-25.669)
      app.streambundle.getSelfStream('navigation.gnss.differentialAge').push(2.0)
      app.streambundle.getSelfStream('navigation.gnss.differentialReference').push('0031')
-  }) */
+     app.streambundle.getSelfStream('navigation.position').push({ longitude: -122.03782631066667, latitude: 37.39109795066667 })
+  }) 
 }) 
