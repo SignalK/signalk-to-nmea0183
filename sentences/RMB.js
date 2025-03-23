c6359b5242fc839b3bceb5855f91c6866d2a0cf9
Fernando Saux <fernando_saux@hotmail.com>
2025-03-23 18:33:55 -0300

    Alignment to NMEA 2.3 spec and correct sk keys

--- sentences/RMB.js
+++ sentences/RMB.js
@@ -1,44 +1,63 @@
 /*
 Heading and distance to waypoint:
-$IIRMB,A,x.x,a,,,IIII.II,a,yyyyy.yy,a,x.x,x.x,x.x,A,a*hh
- I I I I I I I I I_Speed to WP in knots
- I I I I I I I I_True heading to destination in degrees
- I I I I I I I_Distance to destination in miles
- I I I I I_ ___ I_Longitude of the WP to destination, E/W
- I I I__ I_Latidude of the WP to destination, N/S
- I I_Direction of cross-track error, L/R
- I_Distance of cross-track error in miles
+                                                             14
+        1 2   3 4    5    6       7 8        9 10  11  12  13|  15
+        | |   | |    |    |       | |        | |   |   |   | |   |
+ $--RMB,A,x.x,a,c--c,c--c,llll.ll,a,yyyyy.yy,a,x.x,x.x,x.x,A,m,*hh<CR><LF>
+------------------------------------------------------------------------------
+
+Field Number:
+
+1. Status, A= Active, V = Void
+2. Cross Track error - nautical miles
+3. Direction to Steer, Left or Right
+4. TO Waypoint ID
+5. FROM Waypoint ID
+6. Destination Waypoint Latitude
+7. N or S
+8. Destination Waypoint Longitude
+9. E or W
+10. Range to destination in nautical miles
+11. Bearing to destination in degrees True
+12. Destination closing velocity in knots
+13. Arrival Status, A = Arrival Circle Entered
+14. FAA mode indicator (NMEA 2.3 and later)
+15. Checksum
 */
-// to verify
+
 const nmea = require('../nmea.js')
 module.exports = function (app) {
   return {
     sentence: 'RMB',
     title: 'RMB - Heading and distance to waypoint',
     keys: [
-      'navigation.courseRhumbline.crossTrackError',
-      'resources.waypoints.next.position.latitude',
-      'resources.waypoints.next.position.longitude',
-      'navigation.courseRhumbline.nextPoint.distance',
-      'navigation.courseRhumbline.bearingTrue'
+      'navigation.courseGreatCircle.nextPoint',
+      'navigation.courseGreatCircle.crossTrackError',
+      'navigation.courseGreatCircle.nextPoint.distance',
+      'navigation.courseGreatCircle.nextPoint.bearingTrue',
+      'navigation.courseGreatCircle.nextPoint.velocityMadeGood'
     ],
     f: function (
+      nextPoint,
       crossTrackError,
-      wpLatitude,
-      wpLongitude,
       wpDistance,
-      bearingTrue
+      bearingTrue,
+      vmgWpt
     ) {
       return nmea.toSentence([
         '$IIRMB',
-        crossTrackError.toFixed(2),
+        'A',
+        Math.abs(nmea.mToNm(crossTrackError)).toFixed(2),      
         crossTrackError < 0 ? 'R' : 'L',
-        nmea.toNmeaDegreesLatitude(wpLatitude),
-        nmea.toNmeaDegreesLongitude(wpLongitude),
-        wpDistance.toFixed(2),
-        nmea.radsToDeg(bearingTrue).toFixed(2),
-        'V', // dont set the arrival flag as it will set of alarms.
-        ''
+        '',
+        '',
+        nmea.toNmeaDegreesLatitude(nextPoint.position.latitude),
+        nmea.toNmeaDegreesLongitude(nextPoint.position.longitude),
+        Math.abs(nmea.mToNm(wpDistance)).toFixed(2),
+        nmea.radsToPositiveDeg(bearingTrue).toFixed(0),
+        nmea.msToKnots(vmgWpt).toFixed(2),
+        '',
+        'A'
       ])
     }
   }
